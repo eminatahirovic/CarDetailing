cdde032651f5bbc4f33bd28157d36450c0ee03a7
eminatahirovic <emina.tahirovic@stu.ibu.edu.ba>
2025-11-14 22:49:08 +0100

    testing new branch

--- backend/vendor/composer/autoload_psr4.php
+++ backend/vendor/composer/autoload_psr4.php
@@ -11,6 +11,7 @@ return array(
     'Symfony\\Component\\Finder\\' => array($vendorDir . '/symfony/finder'),
     'Psr\\Cache\\' => array($vendorDir . '/psr/cache/src'),
     'OpenApi\\' => array($vendorDir . '/zircote/swagger-php/src'),
+    'Firebase\\JWT\\' => array($vendorDir . '/firebase/php-jwt/src'),
     'Doctrine\\Deprecations\\' => array($vendorDir . '/doctrine/deprecations/src'),
     'Doctrine\\Common\\Lexer\\' => array($vendorDir . '/doctrine/lexer/src'),
     'Doctrine\\Common\\Annotations\\' => array($vendorDir . '/doctrine/annotations/lib/Doctrine/Common/Annotations'),
